faae6732c3153a28d8bb288eb4424144f6abc7a4
SamyC11 <chahbisamy4@gmail.com>
2023-07-14 20:39:34 +0200

    last commit

--- feu02.js
+++ feu02.js
@@ -15,134 +15,110 @@ const isError = (input1, input2) => {
 }
 
 
+const dataFilesExtraction  = (filePath) => {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8') ;
+        const dataArray = data.split("\n").map((element)=> {
+            return element = element.split("");
+        });
+        return dataArray;
+    }catch (err) {
+    console.log("unfindable");
+          process.exit(1);
+    }
+   
+}
 
+const symbolWidth = (symbol) => {
+    let arrayWidth = 0;
+    for (let i = 0; i < symbol.length; i++) {
+        if (symbol[i].length>arrayWidth) {
+            arrayWidth = symbol[i].length
+        }
+    } return arrayWidth;
+}
+
+const findCoordinates = (reference, symbol, symbolWidth) => {
+    const arrayCoordinates = [];
+
+    for (let i = 0; i < reference.length - symbol.length + 1; i++) {
+      for (let j = 0; j < reference[i].length - symbolWidth + 1; j++) {
+        let match = true;
+    
+        for (let k = 0; k < symbol.length; k++) {
+               for (let l = 0; l < symbol[k].length; l++) {
+                       if (
+                       symbol[k][l] !== " " &&
+                       symbol[k][l] !== reference[i + k][j + l]
+                       ) {
+                       match = false;
+                       break;
+                       }
+               }
+               if (!match) {
+                   break;
+               }
+        }
+        if (match) {
+          arrayCoordinates.push(i, j);
+          break;
+        }
+       
+    }
+    if (arrayCoordinates.length>0) {
+           break; 
+   }
+   }
+    return arrayCoordinates;
+}
+
+
+
+const symbolIntegration = (reference, array, position) => {
+    const arrayToDisplay = reference.map(element => element.fill("-"));
+
+        for (let k = position[0],i = 0 ; k < position[0] + array.length, i < array.length; k++, i++) {
+            for (let l = position[1], j = 0; l < position[1] + array[i].length, j < array[i].length; l++, j++) {
+                if (array[i][j]!==" ") {
+                    arrayToDisplay[k][l]=array[i][j];
+                }
+            }
+        }
+        return arrayToDisplay;
+}
 
 //PARSING
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const filePath2 = process.argv[3]
 
-let boardArray;
-try {
-    const data = fs.readFileSync(filePath, 'utf8') ;
-    boardArray = data.split("\n");
-}catch (err) {
-console.log("unfindable");
-      process.exit(1);
-}
+let boardArray  = dataFilesExtraction(process.argv[2]);
 
-let toFindArray;
-try {
-    const data = fs.readFileSync(filePath2, 'utf8') ;
-    toFindArray = data.split("\n");
-}catch (err) {
-console.log("unfindable");
-      process.exit(1);
-}
+let toFindArray = dataFilesExtraction(process.argv[3]);
 
 
 
 //ERROR HANDLING
-let finalBoardArray = boardArray.map((element)=> {
-    return element = element.split("");
-});
 
-let finaltoFindArray = toFindArray.map((element)=> {
-    return element = element.split("");
-});
-
-if (isError(finalBoardArray, finaltoFindArray)) {
+if (isError(boardArray, toFindArray)) {
     console.log("data error");
     process.exit(1);
 }
 
-
 //RESOLUTION
+const toFindArrayWidth = symbolWidth (toFindArray);
 
-//console.log(finalBoardArray);
-//console.log(finaltoFindArray);
-
-
+const symbolCoordinates = findCoordinates(boardArray,toFindArray,toFindArrayWidth);
 
 
+//DISPLAY
 
-//COORDONNEES
-         const arrayTest = [];
-
-         for (let i = 0; i < finalBoardArray.length; i++) {
-           for (let j = 0; j < finalBoardArray[i].length - finaltoFindArray.length + 1; j++) {
-             let match = true;
-         
-             for (let k = 0; k < finaltoFindArray.length; k++) {
-                    for (let l = 0; l < finaltoFindArray[k].length; l++) {
-                            if (
-                            finaltoFindArray[k][l] !== " " &&
-                            finaltoFindArray[k][l] !== finalBoardArray[i + k][j + l]
-                            ) {
-                            match = false;
-                            break;
-                            }
-                    }
-                    if (!match) {
-                        break;
-                    }
-             }
-             if (match) {
-               arrayTest.push([i, j]);
-               break;
-             }
-            
-         }
-         if (arrayTest.length>0) {
-                break; 
-        }
-        }
-         console.log(arrayTest);
-         
-
-//ISPRESENT
-
-if (arrayTest.length>0) {
-    console.log("trouvé sa mère !");
+if (symbolCoordinates.length>0) {
+    console.log(`Trouvé sa mère ! \nCoordonnées : ${symbolCoordinates}`);
+    const finalArray = symbolIntegration(boardArray, toFindArray, symbolCoordinates).map((element) => {
+    return element.join("");
+    });
+    console.log(finalArray.join("\n"));
 }
 else {
-    console.log("j'ai pas trouvéoo !!");
+    console.log("J'ai pas trouvéoo !!");
 }      
-       
-
-
-//Affichage tableau final avec symbole
-
-
-
-const finalArrayDisplay = (reference, array, position) => {
-    const baseOfDisplay = reference.map(element => element.fill("-"));
-    console.log(baseOfDisplay);
-    console.log(position[0][1]);
-
-        for (let k = position[0][0],i = 0 ; k < position[0][0] + array.length, i < array.length; k++, i++) {
-            for (let l = position[0][1], j = 0; l < position[0][1] + array[i].length, j < array[i].length; l++, j++) {
-                baseOfDisplay[k][l]=array[i][j];
-            }
-        }
-
-        return baseOfDisplay;
-}
-const Array1 = finalArrayDisplay(finalBoardArray, finaltoFindArray, arrayTest);
-console.log(Array1);
-
-
-
-
-const finalDisplay = (array) => {
-    return array.map(element => element.join(""));
-};
-
-const finalArray = finalDisplay(Array1);
-console.log(finalArray.join("\n"));
-
-
-
-  
-    
